22750196bce9b433cbc60e1d117a6d36f07acb56
RuslanWorkGit <lulka758@gmail.com>
2023-03-28 11:47:13 +0300

    firts commit

---
+++ Live/FirstLive/FirstLive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstLive
+//
+//  Created by Ruslan Liulka on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the 
+        
+    }
+
+
+}
+
